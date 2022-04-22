8c460f59592cb6640f131905e9b2d7ccb169f059
James Aduke <jamesodwuor4@gmail.com>
2022-04-22 16:37:22 +0300

    add the styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-
+import('./styles.css')
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">383727</div>
+        <div className="current-operand">9438210948</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
     </div>
   );
 }
